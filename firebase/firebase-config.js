35e64552acc04b10522f5b506834e61696d86242
Divya Jyoti Chaudhary <79772150+divya00222@users.noreply.github.com>
2026-07-29 14:53:48 +0545

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -5,15 +5,13 @@ import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getStorage } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js';
 
 export const firebaseConfig = {
-  projectId: "skillful-gear-8dpgw",
-  appId: "1:752152599090:web:7b6c6aa5814e5fb4bbfa2b",
-  apiKey: "AIzaSyCRaMkQV5c5V3nOZGuVIN-nlt4pYfVLQtg",
-  authDomain: "skillful-gear-8dpgw.firebaseapp.com",
-  firestoreDatabaseId: "ai-studio-djchaudharyportf-763d84d6-2164-44d6-82c2-4390c7569014",
-  storageBucket: "skillful-gear-8dpgw.firebasestorage.app",
-  messagingSenderId: "752152599090",
-  oAuthClientId: "752152599090-cmm37fl032lqua0rgo6mot9cd9jb7noc.apps.googleusercontent.com",
-};
+    apiKey: "AIzaSyA4cYzkHspN1nv1iumK3ooAZ3APaDlgRgQ",
+    authDomain: "portfolio-4d3bb.firebaseapp.com",
+    projectId: "portfolio-4d3bb",
+    storageBucket: "portfolio-4d3bb.firebasestorage.app",
+    messagingSenderId: "445507061953",
+    appId: "1:445507061953:web:15bd8b80111ace534aec40",
+    measurementId: "G-5YJFW9DP38"};
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
